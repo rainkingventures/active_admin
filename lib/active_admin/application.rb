257f9cdbed1263cbd61ec868fb8f2e65a27c8101
Matt De Leon <matt@havemousewilltravel.com>
2013-05-27 20:04:12 -0300

    Try commenting out reference to Sass::Plugin

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -253,10 +253,10 @@ module ActiveAdmin
       if ActiveAdmin.use_asset_pipeline?
         # Add our mixins to the load path for SASS
         ::Sass::Engine::DEFAULT_OPTIONS[:load_paths] <<  File.expand_path("../../../app/assets/stylesheets", __FILE__)
-      else
-        require 'active_admin/sass/css_loader'
-        ::Sass::Plugin.add_template_location(File.expand_path("../../../app/assets/stylesheets", __FILE__))
-        ::Sass::Plugin.add_template_location(File.expand_path("../sass", __FILE__))
+      #else
+      #  require 'active_admin/sass/css_loader'
+      #  ::Sass::Plugin.add_template_location(File.expand_path("../../../app/assets/stylesheets", __FILE__))
+      #  ::Sass::Plugin.add_template_location(File.expand_path("../sass", __FILE__))
       end
     end
   end
